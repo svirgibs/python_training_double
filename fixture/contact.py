a57daf1a870212bddb9075c132c360a5adcd8653
Denis Yavorskiy <svirgibs@gmail.com>
2022-07-06 17:36:37 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестов

---
+++ fixture/contact.py
@@ -0,0 +1,25 @@
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.init_add_contact()
+        wd.find_element("name", "firstname").clear()
+        wd.find_element("name", "firstname").send_keys(contact.firstname)
+        wd.find_element("name", "middlename").clear()
+        wd.find_element("name", "middlename").send_keys(contact.middlename)
+        wd.find_element("name", "lastname").clear()
+        wd.find_element("name", "lastname").send_keys(contact.lastname)
+        wd.find_element("xpath", "//div[@id='content']/form/input[21]").click()
+        self.go_to_homepage()
+
+    def init_add_contact(self):
+        wd = self.app.wd
+        wd.find_element("link text", "add new").click()
+
+    def go_to_homepage(self):
+        wd = self.app.wd
+        wd.find_element("link text", "home page").click()
---
+++ test/test_add_contact.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.contact import Contact
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+    
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Denis", middlename="Timurovich", lastname="Iavorskii"))
+    app.session.logout()
+
+
+def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", middlename="", lastname=""))
+    app.session.logout()
