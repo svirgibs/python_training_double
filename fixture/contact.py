5475daab41c92e8561b5ca6a34aae9cf6e49b878
Denis Yavorskiy <svirgibs@gmail.com>
2022-07-07 16:26:40 +0300

    Задание №10: оптимизировать переходы между страницами рефакторинг

--- fixture/contact.py
+++ fixture/contact.py
@@ -14,7 +14,7 @@ class ContactHelper:
         self.return_to_homepage()
 
     def modify_first(self, contact):
-        self.go_to_homepage()
+        self.open_home()
         self.init_modify_first_contact()
         self.app.change_field_value("firstname", contact.firstname)
         self.app.change_field_value("middlename", contact.middlename)
@@ -23,15 +23,15 @@ class ContactHelper:
         self.return_to_homepage()
 
     def delete_first_contact(self):
-        self.go_to_homepage()
+        self.open_home()
         self.select_first_contact()
         self.submit_deletion()
         self.delete_alert_accept()
-        self.go_to_homepage()
+        self.open_home()
 
     def count(self):
         wd = self.app.wd
-        self.go_to_homepage()
+        self.open_home()
         return len(wd.find_elements("name", "selected[]"))
 
     def select_first_contact(self):
@@ -46,9 +46,10 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element("link text", "add new").click()
 
-    def go_to_homepage(self):
+    def open_home(self):
         wd = self.app.wd
-        wd.find_element("link text", "home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements("name", "searchstring")) > 0):
+            wd.find_element("link text", "home").click()
 
     def return_to_homepage(self):
         wd = self.app.wd
--- fixture/group.py
+++ fixture/group.py
@@ -9,25 +9,18 @@ class GroupHelper:
         wd.find_element("link text", "group page").click()
 
     def create(self, group):
-        wd = self.app.wd
         self.open_groups_page()
-        # init group creation
-        wd.find_element("name", "new").click()
+        self.init_group_creation()
         self.fill_group_form(group)
-        # submit group creation
-        wd.find_element("name", "submit").click()
+        self.submit_group_creation()
         self.return_to_groups_page()
 
     def modify_first_group(self, new_group_data):
-        wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
-        # open modification form
-        wd.find_element("name", "edit").click()
-        # fill group form
+        self.open_modification_form()
         self.fill_group_form(new_group_data)
-        # update group modify
-        wd.find_element("name", "update").click()
+        self.submit_update()
         self.return_to_groups_page()
 
     def fill_group_form(self, group):
@@ -36,11 +29,9 @@ class GroupHelper:
         self.app.change_field_value("group_footer", group.footer)
 
     def delete_first_group(self):
-        wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
-        # submit deletion
-        wd.find_element("name", "delete").click()
+        self.submit_deletion()
         self.return_to_groups_page()
 
     def select_first_group(self):
@@ -49,10 +40,30 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        # open groups page
-        wd.find_element("link text", "groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements("name", "new")) > 0):
+            wd.find_element("link text", "groups").click()
 
     def count(self):
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements("name", "selected[]"))
+
+    def init_group_creation(self):
+        wd = self.app.wd
+        wd.find_element("name", "new").click()
+
+    def submit_group_creation(self):
+        wd = self.app.wd
+        wd.find_element("name", "submit").click()
+
+    def open_modification_form(self):
+        wd = self.app.wd
+        wd.find_element("name", "edit").click()
+
+    def submit_update(self):
+        wd = self.app.wd
+        wd.find_element("name", "update").click()
+
+    def submit_deletion(self):
+        wd = self.app.wd
+        wd.find_element("name", "delete").click()
