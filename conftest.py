b1c2b7a55d02e99d79e2d4b2804bdbfcfb5e42d5
Denis Yavorskiy <svirgibs@gmail.com>
2022-07-12 18:01:38 +0300

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -5,13 +5,15 @@ fixture = None
 
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.open_home_page()
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
@@ -25,3 +27,8 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,18 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        else:
+            raise ValueError("Unrezognized browser %s" % browser)
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url =base_url
 
     def is_valid(self):
         try:
@@ -23,7 +29,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/addressbook/"):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def change_field_value(self, field_name, text):
         wd = self.wd
